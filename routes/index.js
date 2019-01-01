46da19e297f01886b1157d83124f77aca784e1fb
numoonchld <raghavendra.saralaya@gmail.com>
2019-01-01 16:50:59 +0530

    complete: admin page bootstrap UI

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Admin Access' });
+  res.render('index', { title: 'ADMIN ACCESS' });
 });
 
 module.exports = router;
