be0ecb04aee47aa8dd93eaabf6bfb1a4a653fc60
numoonchld <raghavendra.saralaya@gmail.com>
2019-01-01 18:45:23 +0530

    routing to admin page works

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'ADMIN ACCESS' });
 });
 
+router.post('/admin', function(req, res, next) {
+  console.log(req.body, req.body.adminname, req.body.password );
+  // TODO: Authetication
+  res.send('the gym selector page will load, (authentication implementation pending)');
+});
+
 module.exports = router;
