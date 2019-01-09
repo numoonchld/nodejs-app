d940c8d11173b5315acbc731ba82a19dfd437600
ClimbZombie <climbzombie@gmail.com>
2019-01-09 21:02:31 +0530

    default secure cookie, force https

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.use(session({
   saveUninitialized: false,
   store: new MongoStore({mongooseConnection: mongoose.connection}),
   cookie: {
-    secure: true,
+    // secure: false,
     maxAge: 1000 * 60 * 30 // 30 minutes cookie
   }
 }))
